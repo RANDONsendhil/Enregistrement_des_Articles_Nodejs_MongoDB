1e51a014333885279a85c02ebea891431a84a17c
Randon <randon2006@gmail.com>
2020-09-06 22:10:02 +0200

    Format document /views/control.js //  SENDHIL

--- views/control.js
+++ views/control.js
@@ -1,76 +1,56 @@
 var app = angular.module('myApp', []);
 
 app.controller('myContr', function ($scope, $http) {
- 
-    
-    
-    
+
     $scope.showitem = false;
     $scope.message = function (page) {
 
-        if( $scope.showitem == false){
-      
-             $scope.showitem = page;   
-         
-        }else{
+        if ($scope.showitem == false) {
+
+            $scope.showitem = page;
+
+        } else {
             $scope.showitem = false;
-            
+
         }
-      
-
-
     }
 
- 
-  $scope.showPage = false;
+    $scope.showPage = false;
     $scope.pageHtml = function () {
-    
-        if( $scope.showPage == false){
-      
-           
-               $scope.showPage = true;
-         
-        }else{
-            $scope.showPage = false;
-            
-        }
-      
 
+        if ($scope.showPage == false) {
+
+            $scope.showPage = true;
+
+        } else {
+            $scope.showPage = false;
+
+        }
 
     }
-    
-    
+
+
     $scope.showPagedel = false;
     $scope.pageHtmldel = function () {
-    
-        if( $scope.showPagedel == false){
-      
-           
-               $scope.showPagedel = true;
-         
-        }else{
-            $scope.showPagedel = false;
-            
-        }
-      
 
+        if ($scope.showPagedel == false) {
+            $scope.showPagedel = true;
+
+        } else {
+            $scope.showPagedel = false;
+
+        }
 
     }
-   
+
     $http.get("article.JSON")
-    .then(function(response){
-       
-  
-        $scope.dataArticles =      response.data ;
-        
-          console.log( 'MYdata'+ response.data );
-    });
-    
-    
- 
-    
-    
+        .then(function (response) {
+
+
+            $scope.dataArticles = response.data;
+
+            console.log('MYdata' + response.data);
+        });
 
 });
 
-      
